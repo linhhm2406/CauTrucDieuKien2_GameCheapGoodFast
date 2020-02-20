cbbc7f632ac3f8e32d4d3398f12f8b6463ed6a27
linhhm24 <linh.hm24@gmail.com>
2020-02-20 23:36:31 +0700

    add

---
+++ cheap.js
@@ -0,0 +1,49 @@
+
+function clickcheapbutton() {
+    let cheap = document.getElementById("clickcheap").checked;
+    // alert(cheap);
+    let good = document.getElementById("clickgood").checked;
+    let fast = document.getElementById("clickfast").checked;
+
+    if(cheap === true){
+        if(good === true){
+            fast=false;
+        } else {fast = document.getElementById("clickfast").checked;
+        }}
+
+    document.getElementById("clickcheap").checked=cheap;
+    document.getElementById("clickgood").checked=good;
+    document.getElementById("clickfast").checked=fast;
+}
+function clickgoodbutton() {
+    let cheap = document.getElementById("clickcheap").checked;
+    // alert(cheap);
+    let good = document.getElementById("clickgood").checked;
+    let fast = document.getElementById("clickfast").checked;
+
+    if(good === true){
+        if(fast === true){
+            cheap=false;
+        } else {cheap = document.getElementById("clickcheap").checked;
+    }}
+
+    document.getElementById("clickcheap").checked=cheap;
+    document.getElementById("clickgood").checked=good;
+    document.getElementById("clickfast").checked=fast;
+}
+
+function clickfastbutton() {
+    let cheap = document.getElementById("clickcheap").checked;
+    // alert(cheap);
+    let good = document.getElementById("clickgood").checked;
+    let fast = document.getElementById("clickfast").checked;
+
+    if(fast === true){
+        if(cheap === true){
+            good=false;
+        } else {good = document.getElementById("clickgood").checked;
+        }}
+    document.getElementById("clickcheap").checked=cheap;
+    document.getElementById("clickgood").checked=good;
+    document.getElementById("clickfast").checked=fast;
+}
